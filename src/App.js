8ece8cbc07a6471bf8606c7d734189fd7e06934d
Madhav <sannapureddymadhav@gmail.com>
2023-04-24 12:24:57 +0530

    add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
      <h1>Hello this is anil</h1>
      <h2>Good morning dude! Welcome</h2>
      {/* <h3>madhav and shyam are frds</h3> */}
+     <h4>today session is 5pm</h4>
     </div>
   );
 }
