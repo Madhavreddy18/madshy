2a7d0e06601aa27145bcce841a145988b640d867
Madhav <sannapureddymadhav@gmail.com>
2023-04-24 12:09:33 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello this is anil</h1>
+     <h2>Good morning dude! Welcome</h2>
+     <h3>madhav and shyam are frds</h3>
+    </div>
+  );
+}
+
+export default App;
